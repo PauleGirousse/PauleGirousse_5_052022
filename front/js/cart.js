ed78551f1f4a7e38228501e4ab711794cead9c1a
Maya <paulegirousse@gmail.com>
2022-07-07 22:35:54 +0200

    formulaire de contact

--- front/js/cart.js
+++ front/js/cart.js
@@ -6,18 +6,17 @@ let article = {};
 let panierFinal = [];
 let canape = {};
 let canapeFinal = {};
-// let nbCanape = [];
-// Pour forcer le rechargement de la page
-function rechargement() {
-  location.reload();
+let nbCanape = [];
+
+// récupère le panier depuis le localstorage et met à jour le dom
+function getLsPanier() {
+  let updatepanier = JSON.parse(localStorage.getItem("lsPanier"));
 }
 
-// recupere le panier depuis le localstorage et met à jour le dom
-function MAJpanierFinal() {
-  let MAJpanierFinal = JSON.parse(localStorage.getItem("lsPanier"));
-  console.log(MAJpanierFinal);
+// envoi du panier dans le localstorage
+function updateLsPanier() {
+  let updateLsPanier = localStorage.setItem("lsPanier", JSON.stringify(panier));
 }
-
 if (panier === null) {
   //si le panier est vide
   alert("le panier est vide");
@@ -27,187 +26,269 @@ if (panier === null) {
   promise01.then((response) => {
     console.log(response);
     const canapes = response.json();
-    canapes.then((data) => {
-      // console.log(data);
-      for (canape of panier) {
-        //pour les canapes du panier, création  des différents articles suivant leur id et leur couleur
-        article = document.createElement("article");
-        article.classList.add("cart__item");
+    canapes.then(
+      (data) => {
+        // console.log(data);
+        for (canape of panier) {
+          //pour les canapes du panier, création  des différents articles suivant leur id et leur couleur
+          article = document.createElement("article");
+          article.classList.add("cart__item");
 
-        article.setAttribute("data-id", canape._id);
-        article.setAttribute("data-color", canape.color);
-        console.log(canape);
+          article.setAttribute("data-id", canape._id);
+          article.setAttribute("data-color", canape.color);
+          // console.log(canape);
 
-        // boucle sur les canapés du panier pour rechercher le même id dans les canapés de l'API
-        let rechercheProduitApi = data.find(
-          (produit) => produit._id === canape._id
-        );
-        // console.log(rechercheProduitApi);
+          // boucle sur les canapés du panier pour rechercher le même id dans les canapés de l'API
+          let rechercheProduitApi = data.find(
+            (produit) => produit._id === canape._id
+          );
+          // console.log(rechercheProduitApi);
 
-        //création de l'article combiné des attributs de l'API et du panier
-        let canapeFinal = {
-          _id: canape._id,
-          imageUrl: rechercheProduitApi.imageUrl,
-          altTxt: rechercheProduitApi.altTxt,
-          name: rechercheProduitApi.name,
-          color: canape.color,
-          price: rechercheProduitApi.price,
-          quantity: canape.quantity,
-        };
-        // insertion dans le panier final des canapés combinés
-        panierFinal.push(canapeFinal);
-        // Création de l'affichage dynamique du panier
-        let cart__item__img = document.createElement("div");
-        cart__item__img.classList.add("cart__item__img");
+          //création de l'article combiné des attributs de l'API et du panier
+          let canapeFinal = {
+            _id: canape._id,
+            imageUrl: rechercheProduitApi.imageUrl,
+            altTxt: rechercheProduitApi.altTxt,
+            name: rechercheProduitApi.name,
+            color: canape.color,
+            price: rechercheProduitApi.price,
+            quantity: canape.quantity,
+          };
+          // products = localStorage.setItem(
+          //   "lsProducts",
+          //   JSON.stringify(products)
+          // );
+          // insertion dans le panier final des canapés complets
+          panierFinal.push(canapeFinal);
 
-        article.appendChild(cart__item__img);
+          // Création de l'affichage dynamique du panier
+          let cart__item__img = document.createElement("div");
+          cart__item__img.classList.add("cart__item__img");
 
-        let img = document.createElement("img");
-        img.setAttribute("src", canapeFinal.imageUrl);
-        img.setAttribute("alt", canapeFinal.altTxt);
+          article.appendChild(cart__item__img);
 
-        cart__item__img.appendChild(img);
+          let img = document.createElement("img");
+          img.setAttribute("src", canapeFinal.imageUrl);
+          img.setAttribute("alt", canapeFinal.altTxt);
 
-        let cart__item__content = document.createElement("div");
-        cart__item__content.classList.add("cart__item__content");
+          cart__item__img.appendChild(img);
 
-        article.appendChild(cart__item__content);
+          let cart__item__content = document.createElement("div");
+          cart__item__content.classList.add("cart__item__content");
 
-        let description = document.createElement("div");
-        description.classList.add("cart__item__content__description");
+          article.appendChild(cart__item__content);
 
-        cart__item__content.appendChild(description);
+          let description = document.createElement("div");
+          description.classList.add("cart__item__content__description");
 
-        let nom = document.createElement("h2");
-        nom.innerText = canapeFinal.name;
-        description.appendChild(nom);
-        let couleur = document.createElement("p");
-        couleur.innerText = canapeFinal.color;
-        description.appendChild(couleur);
-        let prix = document.createElement("p");
-        prix.innerText = canapeFinal.price + " " + "€";
-        description.appendChild(prix);
+          cart__item__content.appendChild(description);
 
-        let cart__item__content__settings = document.createElement("div");
-        cart__item__content__settings.classList.add(
-          "cart__item__content__settings"
-        );
+          let nom = document.createElement("h2");
+          nom.innerText = canapeFinal.name;
+          description.appendChild(nom);
+          let couleur = document.createElement("p");
+          couleur.innerText = canapeFinal.color;
+          description.appendChild(couleur);
+          let prix = document.createElement("p");
+          prix.innerText = canapeFinal.price + " " + "€";
+          description.appendChild(prix);
 
-        cart__item__content.appendChild(cart__item__content__settings);
+          let cart__item__content__settings = document.createElement("div");
+          cart__item__content__settings.classList.add(
+            "cart__item__content__settings"
+          );
 
-        let cart__item__content__settings__quantity =
-          document.createElement("div");
-        cart__item__content__settings__quantity.classList.add(
-          "cart__item__content__settings__quantity"
-        );
+          cart__item__content.appendChild(cart__item__content__settings);
 
-        cart__item__content__settings.appendChild(
-          cart__item__content__settings__quantity
-        );
+          let cart__item__content__settings__quantity =
+            document.createElement("div");
+          cart__item__content__settings__quantity.classList.add(
+            "cart__item__content__settings__quantity"
+          );
 
-        let quantite = document.createElement("p");
-        quantite.innerText = "Qté :";
-        cart__item__content__settings__quantity.appendChild(quantite);
+          cart__item__content__settings.appendChild(
+            cart__item__content__settings__quantity
+          );
 
-        let input = document.createElement("input");
-        input.classList.add("itemQuantity");
-        input.setAttribute("type", "number");
-        input.setAttribute("name", "itemQuantity");
-        input.setAttribute("min", "1");
-        input.setAttribute("max", "100");
-        input.setAttribute("value", canapeFinal.quantity);
+          let quantite = document.createElement("p");
+          quantite.innerText = "Qté :";
+          cart__item__content__settings__quantity.appendChild(quantite);
 
-        cart__item__content__settings__quantity.appendChild(input);
+          let input = document.createElement("input");
+          input.classList.add("itemQuantity");
+          input.setAttribute("type", "number");
+          input.setAttribute("name", "itemQuantity");
+          input.setAttribute("min", "1");
+          input.setAttribute("max", "100");
+          input.setAttribute("value", canapeFinal.quantity);
 
-        let cart__item__content__settings__delete =
-          document.createElement("div");
-        cart__item__content__settings__delete.classList.add(
-          "cart__item__content__settings__delete"
-        );
+          cart__item__content__settings__quantity.appendChild(input);
 
-        cart__item__content__settings.appendChild(
-          cart__item__content__settings__delete
-        );
+          let cart__item__content__settings__delete =
+            document.createElement("div");
+          cart__item__content__settings__delete.classList.add(
+            "cart__item__content__settings__delete"
+          );
 
-        let deleteItem = document.createElement("p");
-        deleteItem.classList.add("deleteItem");
-        deleteItem.innerText = "Supprimer";
-        cart__item__content__settings__delete.appendChild(deleteItem);
-        //insertion dans le HTML
-        items.appendChild(article);
+          cart__item__content__settings.appendChild(
+            cart__item__content__settings__delete
+          );
 
-        // Changer la quantité de canapés
-        input.addEventListener("change", changeQuantity);
-        function changeQuantity() {
-          canapeFinal.quantity = parseInt(this.value);
+          let deleteItem = document.createElement("p");
+          deleteItem.classList.add("deleteItem");
+          deleteItem.innerText = "Supprimer";
+          cart__item__content__settings__delete.appendChild(deleteItem);
 
-          console.log(canapeFinal.quantity);
-          console.log(canapeFinal);
-          //dans le DOM
+          //insertion dans le HTML
+          items.appendChild(article);
+
+          // Changer la quantité de canapés
           let inp = document.querySelector("input");
-          let rangArticle = inp.closest("article");
-          console.log(rangArticle);
-          rangArticle.dataset.quantity = canapeFinal.quantity;
-          //dans le localstorage
-          canape.quantity = canapeFinal.quantity;
-          console.log(panier);
-          localStorage.setItem("lsPanier", JSON.stringify(panier));
-          // let item = document.querySelector("cart_item");
-          // console.log(item.$(data.id));
-          // item.dataset.quantity.value = canapeFinal.quantity;
-          // console.log(item.dataset.quantity.value);
-          // localStorage.setItem("lsPanier", JSON.stringify(panierFinal)); //envoi du nouveau panier dans le localstorage
-          // MAJpanierFinal();
+
+          input.addEventListener("change", changeQuantity);
+          function changeQuantity() {
+            let rangArticle = inp.closest("article");
+            canapeFinal.quantity = parseInt(this.value);
+            canape.quantity = canapeFinal.quantity;
+            console.log(canape.quantity);
+            console.log(panier);
+            updateLsPanier();
+            // function changeQuantity() {
+            //   let rangArticle = inp.closest("article");
+            //   console.log(rangArticle);
+            // e.target._id = canape._id;
+            // e.target.color = canape.color;
+            // canape.quantity = parseInt(this.value);
+            // console.log(canape);
+
+            //   canape.quantity = canapeFinal.quantity;
+
+            //   totalNbCanapes += nbCanape;
+            //   console.log(totalNbCanapes);
+            //   localStorage.setItem(
+            //     "lsTotalQuantity",
+            //     JSON.stringify(totalNbCanapes)
+            //   );
+            //   totalQuantity.innerText = totalNbCanapes;
+            nbArticles();
+            price();
+          }
+          updateLsPanier();
+
+          //Affichage de la quantité des articles
+          function nbArticles() {
+            let totalNbCanapes = 0;
+            let totalQuantity = document.querySelector("#totalQuantity");
+            let quantityArray = panierFinal.map((item) => item.quantity);
+            console.log(quantityArray);
+            for (let i = 0; i < quantityArray.length; i++) {
+              totalNbCanapes += quantityArray[i];
+              // console.log(totalNbCanapes);
+              totalQuantity.innerText = totalNbCanapes;
+              // localStorage.setItem(
+              //   "lsTotalQuantity",
+              //   JSON.stringify(totalNbCanapes)
+              // );
+            }
+          }
+          nbArticles();
+          // Affichage du prix total
+
+          function price() {
+            let canapePrice = 0;
+            let priceArray = [];
+            let total = 0;
+            let totalPrice = document.querySelector("#totalPrice");
+
+            panierFinal.forEach((canapeFinal) => {
+              canapePrice = canapeFinal.price * canapeFinal.quantity;
+
+              priceArray.push(canapePrice);
+              // console.log(priceArray);
+              total += canapePrice;
+              // console.log(total);
+              totalPrice.innerText = total.toFixed(2);
+              // localStorage.setItem("lsTotalPrice", JSON.stringify(total));
+            });
+          }
         }
+        price();
       }
-    });
+      // });
+    );
+  });
+}
+//recuperer l'ID des articles dans un tableau pour le POST
+let products = panier.map((item) => item._id);
+
+console.log(products);
+
+let order = document.querySelector("#order");
+let lsFormulaire = {};
+let domFormulaire = {};
+let error = "Format incorrect";
+let firstName = document.querySelector("#firstName");
+
+let firstName_m = document.querySelector("#firstNameErrorMsg");
+
+let firstName_v = /^[a-zA-Zéèïî][a-zéèàçïî]+([-'\s][a-zA-Zéèîï][a-zéèêàçîï]+)?/;
+let lastName = document.querySelector("#lastName");
+let lastName_m = document.querySelector("#lastNameErrorMsg");
+let lastName_v = /^[a-zA-Zéèîï]+([[a-zA-Zéèëêïîàç]+)/;
+let address = document.querySelector("#address");
+let address_m = document.querySelector("#addressErrorMsg");
+let address_v = /^[a-zA-Z0-9]+[a-zA-Z0-9éèêëîïàç,.°-]+/;
+let city = document.querySelector("#city");
+let city_m = document.querySelector("#cityErrorMsg");
+let city_v = /^[0-9]{5}/;
+let email = document.querySelector("#email");
+let email_m = document.querySelector("#emailErrorMsg");
+let email_v = /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}/;
+
+let jsonBody = [
+  products,
+  (contact = JSON.parse(localStorage.getItem("lsFormulaire"))),
+];
+console.log(jsonBody);
+
+order.addEventListener("click", valid);
+function valid(e) {
+  e.preventDefault();
+  let formulaire = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    address: address.value,
+    city: city.value,
+    email: email.value,
+  };
+  if (firstName_v.test(firstName.value) === false) {
+    firstName_m.innerText = error;
+
+    console.log(firstName_m);
+  } else if (lastName_v.test(lastName.value) === false) {
+    lastName_m.innerText = error;
+  } else if (address_v.test(address.value) === false) {
+    address_m.innerText = error;
+  } else if (city_v.test(city.value) === false) {
+    city_m.innerText = error;
+  } else if (email_v.test(email.value) === false) {
+    email_m.innerText = error;
+  } else {
+    lsFormulaire = localStorage.setItem(
+      "lsFormulaire",
+      JSON.stringify(formulaire)
+    );
+  }
+  fetch("http://localhost:3000/api/products", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(jsonBody),
   });
 }
 
-let totalQuantity = document.querySelector("#totalQuantity");
-let totalPrice = document.querySelector("totalPrice");
-// console.log(totalPrice);
-let prixCanapes = 0;
-let totalNbCanapes = 0;
-
-const quantityArray = panierFinal.map((item) => item.quantity);
-
-console.log(quantityArray);
-
-for (let i = 0; i < quantityArray.length; i++) {
-  totalNbCanapes += quantityArray[i];
-}
-console.log(totalNbCanapes);
-totalQuantity.innerText = totalNbCanapes;
-localStorage.setItem("lsTotalQuantity", JSON.stringify(totalNbCanapes));
-
-let canapePrice = canapeFinal.price * canapeFinal.quantity;
-console.log(typeof canapePrice);
-
-let PriceArray = [];
-PriceArray.push(canapePrice);
-console.log(PriceArray);
-
-//   let nbCanape = parseInt(canapeFinal.quantity);
-//   totalNbCanapes += nbCanape;
-//   // console.log(totalNbCanapes);
-//   localStorage.setItem("lsTotalQuantity", JSON.stringify(totalNbCanapes));
-//   totalQuantity.innerText = totalNbCanapes;
-
-//   prixCanapes += canapePrice;
-//   console.log(prixCanapes);
-//   localStorage.setItem("lsTotalPrice", JSON.stringify(prixCanapes));
-// }
-// totalPrice.innerText = prixCanapes;
-
-// console.log(prixTotalcanape);
-
-// console.log(totalPrice);
-// canapeFinal.price = parseInt(canapeFinal.price, 10); //pour additionner, conversion de string en number
-
-// totalPrice.innerText;
-
-// debugger;
 //supprime l'article dans le panier
 
 // let supprimer = document.querySelector("deleteItem");
@@ -238,3 +319,16 @@ console.log(PriceArray);
 // panierFinal = JSON.parse(localStorage.getItem("lsPanier", panierFinal));
 
 // panierFinal = JSON.parse(localStorage.setItem("lsPanier", panierFinal));
+
+// localStorage.setItem("lsPanier", JSON.stringify(panier));
+// let item = document.querySelector("cart_item");
+// console.log(item.$(data.id));
+// item.dataset.quantity.value = canapeFinal.quantity;
+// console.log(item.dataset.quantity.value);
+// localStorage.setItem("lsPanier", JSON.stringify(panierFinal)); //envoi du nouveau panier dans le localstorage
+// MAJpanierFinal();
+
+// for (canapeFinal of panierFinal) {
+//   console.log(panierFinal);
+// let canapePrice = canapeFinal.price * canapeFinal.quantity;
+// console.log(canapePrice);
